28f1a7afc44e720e02b7b394758312ca168f8fe3
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2025-09-12 23:17:51 +0500

    Create Refresh Access Token API

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/auth.controller.js";
 import verifyJwt from "../utils/vertifyJwt.js";
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", verifyJwt, logout);
+router.post("/refresh-token", refreshToken);
 
 export default router;
